6de8dcc220c5b0c02fa11066b3dcf7c9ecc99ebf
dsolerh <dsolerh.cinter95@gmail.com>
2022-01-28 15:29:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,28 +9,33 @@ mongoose.connect(
 
 run();
 async function run() {
+  console.clear();
   // const u1 = new User({ name: "Krla", age: 26 });
   // await u1.save();
   try {
-    // const u2 = await User.create({
-    //   name: "Dany",
-    //   age: "scac4",
-    //   address: {
-    //     street: "170 st",
-    //     province: "123",
-    //     city: "La hanama",
-    //   },
-    // });
+    const u2 = await User.create({
+      name: "Dany",
+      age: "12",
+      address: {
+        street: "170 st",
+        province: "123",
+        city: "La hanama",
+      },
+    });
     // console.log(u2);
     const usr = await User.find();
-    console.clear();
+    // console.log("🚀 ~ file: index.js ~ line 26 ~ run ~ usr", usr)
+    // console.clear();
+    const r = await User.findByIdAndUpdate("61f43ac421913dd44b99d377", {name: 'Soler'});
+    console.log("🚀 ~ file: index.js ~ line 30 ~ run ~ r", r);
     for (const u of usr) {
+      // console.log("🚀 ~ file: index.js ~ line 30 ~ run ~ u", u);
       // console.log(u.address);
-      console.log(u.file);
+      // const r = await u.update({name: "KK"});
+      // console.log(r);
       // console.log(u.createdAt);
     }
     // console.log("🚀 ~ file: index.js ~ line 26 ~ run ~ usr", usr)
-    
   } catch (error) {
     console.log(error);
   }
